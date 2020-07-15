433ca0ac8f321c47c3bd00e944be8fbbea0746ef
Jorge Venegas <brangelina01@gmail.com>
2020-07-15 05:56:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   x = 0 
   new_array = []
-  if block_given?
+    if block_given?
     new_array << yield(array[x])
   end 
-  x += 1 
+    x += 1 
   end 
   puts new_array.inspect 
   new_array
