573fe8cb4883aa76f3b0a2b1310cc809df2d3e09
Jorge Venegas <brangelina01@gmail.com>
2020-07-15 05:50:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,11 @@
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-
-my_collect(collection) do |lang|
-  lang.upcase
-end
+def my_collect(array)
+  x = 0 
+  new_array= []
+  if block_given?
+    new_array << yield(array[x])
+  end 
+  x += 1 
+  end 
+  puts new_array.inspect 
+  new_array
+end 
